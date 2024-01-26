477af322085dda78fc5e600795c23798f0f01a80
Matt Harlow <mharlow85@gmail.com>
2024-01-26 17:34:23 -0500

    Imports express and pg modules, creating pool to access local database on Docker.
    Allows client to obtain all artist data in artists database table

---
+++ app.js
@@ -0,0 +1,37 @@
+import express from "express";
+import pg from "pg";
+
+const pool = new pg.Pool({
+    host: 'localhost',
+    port: 6432,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'music'
+});
+
+const PORT = 8004;
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/music", (req, res, next) => {
+    pool.query(`SELECT * FROM artists`)
+        .then((data) => {
+            console.log("All artists: \n", data.rows);
+            res.json(data.rows);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(500);
+        });
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.sendStatus(500);
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
