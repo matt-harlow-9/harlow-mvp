d3ae6ced0b12d8181889e201b879ab8c43bd4a93
Matt Harlow <mharlow85@gmail.com>
2024-01-26 16:44:45 -0500

    Create genre and artist tables for migration into database

---
+++ db/migration.sql
@@ -0,0 +1,12 @@
+CREATE TABLE genres (
+    id SERIAL PRIMARY KEY,
+    genre TEXT,
+)
+
+CREATE TABLE artists (
+    id SERIAL PRIMARY KEY,
+    stage_name TEXT,
+    real_name TEXT,
+    age INTEGER,
+    genre_id INTEGER REFERENCES genres(genre)
+)
